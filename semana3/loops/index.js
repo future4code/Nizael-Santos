bf5afd7f7f348c843fb2aef090ac5c0739ce7177
NNevesSantos <Nizael.Neves2@Gmail.Com>
2020-04-02 09:00:42 -0300

    exercício aula 11

---
+++ semana3/loops/index.js
@@ -0,0 +1,89 @@
+// exercício 1 //
+/*
+let sum = 0
+for(let i = 0; i < 15; i++) {
+  sum += i
+}
+console.log(sum)
+*/ 
+
+/*  rodando o programa para que a condição seja satisfeita , neste caso 15 , e ao fazer isso 
+incrementando +1 e fazendo a soma do resultado do incremento + variável sum.*/    
+
+// 105 // 
+
+// exercício 2 // 
+
+/*const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+const novaLista = []
+const numero = 5
+for(const item of lista){
+  if(item%numero === 0) {
+    novaLista.push(item)
+  }
+}
+console.log(novaLista)*/ 
+
+// a // 
+/* adiciona um elemento,item, no array. 
+
+// b // 
+array (4)[ 10,15,25,30 ] múltiplos de 5 
+
+// c //  
+
+array (6)[ 12,15,18,21,27,30 ] múltiplos de 3 
+array (1)[ 12 ] múltiplos de 4 
+
+*/ 
+
+// exercício 3 //  
+
+
+// a //
+
+/*
+const numeros = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55] 
+let maior = numeros[0] 
+let menor = numeros[0]
+    
+for(let inc = 0 ; inc < 12 ; inc++) { 
+        const novovalor = numeros[inc]
+        
+        if(novovalor > maior) {
+            maior = novovalor
+        } 
+        if(novovalor < menor) 
+            menor = novovalor
+} 
+
+console.log(" O maior número é " + maior + " e o menor é " + menor)
+*/
+
+// b //  
+
+// const numeros = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55] 
+// let arraynova = [0]
+
+// while(arraynova === numeros[12]) { 
+//     numeros = numeros / 10 
+//         console.log(arraynova)
+//     }
+
+
+
+
+// c // 
+
+// const numeros = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55] 
+// let evens = [];
+
+// for(let i = 0; i < numeros.lenght; i++) {
+//     if((nums[i] % 2) === 1) {   
+//         odds.push(nums[i]);
+//     }
+// } 
+// console.log(evens)
+
+
+// d //
